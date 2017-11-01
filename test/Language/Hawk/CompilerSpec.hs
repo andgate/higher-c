394ae14257fd39de72fb92d7729dadd2efa914e9
Gabriel Anderson <gabe4k@gmail.com>
2017-11-01 04:34:06 -0700

    testing works again

--- test/Language/Hawk/CompilerSpec.hs
+++ test/Language/Hawk/CompilerSpec.hs
@@ -2,6 +2,7 @@ module Language.Hawk.CompilerSpec (main, spec) where
 
 import Test.Hspec
 import Language.Hawk.Compile
+import Language.Hawk.Compile.Config
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
 -- not needed for automatic spec discovery.
@@ -11,4 +12,13 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "hawk compiler" $ do
-    it "can compile code" $ True
+    it "can compile code" $ do
+      hkc $ HkcConfig
+            { _hkcSrcFiles = ["example/Example.hk", "example/Fib.hk"]
+            , _hkcOutFile = ""
+            , _hkcProd = undefined
+            , _hkcExAst = []
+            , _hkcExLib = []
+            , _hkcOpts = undefined
+            }
+      
